c698ced37f7993eb6f2fe37486473d396963690a
James Silberbauer <jamessil@telkomsa.net>
2020-05-08 11:20:50 +0200

    Deploy for Sitrusrand and bump cap version

---
+++ config/deploy/sr.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+# Sitrusrand
+server '192.168.2.6', user: 'nspack', roles: %w[app db web]
+set :deploy_to, '/home/nspack/jasper_reports'
+set :chruby_ruby, 'ruby-2.5.8'
