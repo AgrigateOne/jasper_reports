73c605902766601b128d946dfb94a28c00601863
James Silberbauer <jamessil@telkomsa.net>
2022-01-29 11:26:46 +0200

    Add MC and CFG as deploy targets; update UM target

--- config/deploy/ui_matrooz.rb
+++ config/deploy/ui_matrooz.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
-server '192.168.100.241', user: 'nspack', roles: %w[app db web]
+server '172.18.0.20', user: 'nspack', roles: %w[app db web]
 set :deploy_to, '/home/nspack/jasper_reports'
 set :chruby_ruby, 'ruby-2.5.5'
