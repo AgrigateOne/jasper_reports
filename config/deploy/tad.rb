9b44bb8dc8fc79feca57c66442902630a9921af9
Jonathan W. Vos <jonathanwvos@gmail.com>
2024-12-02 11:18:48 +0200

    Added TAD deploy files.

--- config/deploy/tad.rb
+++ config/deploy/tad.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# VM instance at CFG
+# VM instance at TAD
 server '172.18.0.111', user: 'nspack', roles: %w[app db web]
 set :deploy_to, '/home/nspack/jasper_reports'
-set :chruby_ruby, 'ruby-2.5.8'
+set :chruby_ruby, 'ruby-3.3.5'
