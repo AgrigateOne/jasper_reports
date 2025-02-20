0cb98bbe3c75e0c796bda821eed89a11e5a88649
James Silberbauer <jamessil@telkomsa.net>
2025-02-20 11:43:43 +0200

    Staging and KR deploy config changes

--- config/deploy/kromco.rb
+++ config/deploy/kromco.rb
@@ -1,5 +1,5 @@
 # frozen_string_literal: true
 
 # NSpack server at Kromco.
-server '172.16.16.22', user: 'nsld', roles: %w[app db web]
-set :deploy_to, '/home/nsld/jasper_reports'
+server '172.16.16.22', user: 'onepack', roles: %w[app db web]
+set :deploy_to, '/home/onepack/jasper_reports'
